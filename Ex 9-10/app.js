738108f4f9a75b324915fa5f7b28058fe820734e
MultiMatt21 <mat_oli21@hotmail.com>
2025-09-22 00:37:08 -0300

    Algumas alterações
    Substituição dos exercícios antigos feitos  agora, com function.
    22/09/25 00:36

--- Ex 9-10/app.js
+++
@@ -1,9 +0,0 @@
-// Exercício 9
-
-console.log(3*3*3)
-document.getElementById('IX').innerHTML = 3*3*3
-// alert (3*3*3)
-
-// exercício 10
-console.log('Meu nome é HAHAHAHA')
-document.getElementById('X').innerHTML = ('HAHAHAHAHAHH')
--- Exercício/app.js
+++
@@ -1,5 +0,0 @@
-let n1=parseFloat(prompt('Sua conta deu:'))
-let n2=n1*0.1
-
-document.getElementById('x').innerHTML=`<p>
-O valor total da conta com adição da gorjeta será: ${n1+n2}`
